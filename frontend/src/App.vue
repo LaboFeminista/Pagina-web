997d521c26d3c3cac147cc2176f95e959bafaf2f
Laura Gavilan <lauragavilansabiote@hotmail.com>
2026-04-24 20:50:41 +0200

    creada componente tarjeta dinamica

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import TarjetaDinamica from './components/TarjetaDinamica.vue';
 </script>
 
 <template>
-  <HelloWorld />
+  <TarjetaDinamica/>
+  
 </template>
